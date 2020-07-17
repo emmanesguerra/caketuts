c92810a06b5c5ad81e2857fa603067bf2f081821
emmanesguerra <emman.esguerra2013@gmail.com>
2020-07-17 20:18:06 +0800

    add article policy

---
+++ cms/src/Policy/ArticlePolicy.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Policy;
+
+use App\Model\Entity\Article;
+use Authorization\IdentityInterface;
+
+/**
+ * Article policy
+ */
+class ArticlePolicy
+{
+    /**
+     * Check if $user can create Article
+     *
+     * @param Authorization\IdentityInterface $user The user.
+     * @param App\Model\Entity\Article $article
+     * @return bool
+     */
+    public function canCreate(IdentityInterface $user, Article $article)
+    {
+    }
+
+    /**
+     * Check if $user can update Article
+     *
+     * @param Authorization\IdentityInterface $user The user.
+     * @param App\Model\Entity\Article $article
+     * @return bool
+     */
+    public function canUpdate(IdentityInterface $user, Article $article)
+    {
+    }
+
+    /**
+     * Check if $user can delete Article
+     *
+     * @param Authorization\IdentityInterface $user The user.
+     * @param App\Model\Entity\Article $article
+     * @return bool
+     */
+    public function canDelete(IdentityInterface $user, Article $article)
+    {
+        return $this->isAuthor($user, $article);
+    }
+
+    /**
+     * Check if $user can view Article
+     *
+     * @param Authorization\IdentityInterface $user The user.
+     * @param App\Model\Entity\Article $article
+     * @return bool
+     */
+    public function canView(IdentityInterface $user, Article $article)
+    {
+    }
+    
+    public function canAdd(IdentityInterface $user, Article $article)
+    {
+        // All logged in users can create articles.
+        return true;
+    }
+
+    public function canEdit(IdentityInterface $user, Article $article)
+    {
+        // logged in users can edit their own articles.
+        return $this->isAuthor($user, $article);
+    }
+
+    protected function isAuthor(IdentityInterface $user, Article $article)
+    {
+        return $article->user_id === $user->getIdentifier();
+    }
+}
