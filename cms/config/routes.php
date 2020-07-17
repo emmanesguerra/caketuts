3faa8531d4069b39ca00685ae72abce90258be2c
emmanesguerra <emman.esguerra2013@gmail.com>
2020-07-17 18:08:22 +0800

    remove CSRF protection

--- cms/config/routes.php
+++ cms/config/routes.php
@@ -1,15 +1,10 @@
 <?php
-use Cake\Http\Middleware\CsrfProtectionMiddleware;
 use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 
 $routes->setRouteClass(DashedRoute::class);
 
 $routes->scope('/', function (RouteBuilder $builder) {
-    $builder->registerMiddleware('csrf', new CsrfProtectionMiddleware([
-        'httpOnly' => true,
-    ]));
-    $builder->applyMiddleware('csrf');
     $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
     $builder->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
