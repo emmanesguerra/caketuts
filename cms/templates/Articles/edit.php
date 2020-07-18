a7edcb7b7834e8957e6c1fc7d19444a1f63cbeee
emmanesguerra <emman.esguerra2013@gmail.com>
2020-07-18 09:57:52 +0800

    change encoding of tags from free text to dropdown/multiple

--- cms/templates/Articles/edit.php
+++ cms/templates/Articles/edit.php
@@ -2,7 +2,7 @@
 <?php
     echo $this->Form->create($article);
     echo $this->Form->control('title');
-    echo $this->Form->control('tag_string', ['type' => 'text']);
+    echo $this->Form->control('tags._ids', ['options' => $tags, 'style' => 'height: 100px']);
     echo $this->Form->control('body', ['rows' => '3']);
     echo $this->Form->button(__('Save Article'));
     echo $this->Form->end();
