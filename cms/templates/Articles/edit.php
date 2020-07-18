c8bbe25faac3660de5b1dc8f601743561c790d86
emmanesguerra <emman.esguerra2013@gmail.com>
2020-07-18 12:04:54 +0800

    add id=ckeditor

--- cms/templates/Articles/edit.php
+++ cms/templates/Articles/edit.php
@@ -3,7 +3,7 @@
     echo $this->Form->create($article);
     echo $this->Form->control('title');
     echo $this->Form->control('tags._ids', ['options' => $tags, 'style' => 'height: 100px']);
-    echo $this->Form->control('body', ['rows' => '3']);
+    echo $this->Form->control('body', ['rows' => '3', 'id'=>'ckeditor']);
     echo $this->Form->button(__('Save Article'));
     echo $this->Form->end();
 ?>
