d1b6947bfa5f46e6dfaba3fc262b4b2f07b98527
emmanesguerra <emman.esguerra2013@gmail.com>
2020-07-20 09:45:31 +0800

    add space between ckeditor textarea and button

--- cms/templates/Articles/edit.php
+++ cms/templates/Articles/edit.php
@@ -4,6 +4,6 @@
     echo $this->Form->control('title');
     echo $this->Form->control('tags._ids', ['options' => $tags, 'style' => 'height: 100px']);
     echo $this->Form->control('body', ['rows' => '3', 'id'=>'ckeditor']);
-    echo $this->Form->button(__('Save Article'));
+    echo $this->Form->button(__('Save Article'), ['style' => 'margin-top: 15px']);
     echo $this->Form->end();
 ?>
